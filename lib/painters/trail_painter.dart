b7fed27c702e669977414bbdcd4a16c195552744
unknown <phananhkhoa0123@gmail.com>
2026-04-03 13:45:42 +0700

    fix (trail_painter.dart): resolve only one tail display on screen +  Smoothen the trail to a path instead of dicrete points

--- lib/painters/trail_painter.dart
+++ lib/painters/trail_painter.dart
@@ -5,7 +5,7 @@ class TrailPainter extends CustomPainter {
   final Map<int, List<TrailPoint>> fingers;
   final Set<int> activePointers;
   final int timeToLive;
-  final double MAX_CURSOR_SIZE = 30;  //set độ lớn cursor
+  final double MAX_CURSOR_SIZE = 40;  //set độ lớn cursor
 
   TrailPainter({
     required this.fingers,
@@ -18,70 +18,135 @@ class TrailPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     int currentTime = DateTime.now().millisecondsSinceEpoch;
     
-    // dọn dẹp RAM (?)
-    for (var entry in fingers.entries) {
-      var trail = entry.value;
-      trail.removeWhere((p) => currentTime - p.creationTime > timeToLive);
-    }
-
     // khởi tạo cọ vẽ
-    final paint = Paint()..style = PaintingStyle.fill;
+    final paint = Paint();
+    paint.style = PaintingStyle.fill;
 
-    for (var entry in fingers.entries) {
-      int pointerId = entry.key;
-      var trail = entry.value;
+    for (MapEntry<int, List<TrailPoint>> entry in fingers.entries) {
+      int pointerId = entry.key;              //entry: Map<int, List<TrailPoint>>
+      List<TrailPoint> trail = entry.value;
       bool isCurrentlyPressed = activePointers.contains(pointerId);
 
-      for (int i = 0; i < trail.length; i++) {
-        var point = trail[i];
-        bool isHead = isCurrentlyPressed && (i == trail.length - 1);
-        double lifeLeft;
+      if (isCurrentlyPressed && trail.isNotEmpty) {
+        //xóa các điểm trên trail khi quá thời gian (trừ điểm đầu)
+        trail.removeWhere((p) => (currentTime - p.creationTime > timeToLive && p != trail.last));
+      } else {
+        //nhấc tay lên: xóa hết các điểm trên trail
+        trail.removeWhere((p) => currentTime - p.creationTime > timeToLive);
+      }
 
-        if (isHead) {
-          lifeLeft = 1.0; 
-          point.creationTime = currentTime; 
-          final Gradient headGradient = RadialGradient(
-            colors: [
-              // const Color.fromARGB(255,251,251,251),
-              // const Color.fromARGB(255,255,163,34),
-              // const Color.fromARGB(255,187,20,20),
-              // const Color.fromARGB(255, 0, 58, 107),
-              const Color.fromARGB(255,251,251,251),
-              const Color.fromARGB(150,251,251,251),
-              const Color.fromARGB(0,251,251,251),
-            ],
-            //stops: const [0.0, 0.5, 1.0], 
-          );
+      // for (int i = 0; i < trail.length; i++) {
+      //   TrailPoint point = trail[i];
+      //   bool isHead = isCurrentlyPressed && (i == trail.length - 1);
+      //   double lifeLeft;
 
-          paint.shader = headGradient.createShader(
-            Rect.fromCircle(center: point.position, radius: MAX_CURSOR_SIZE), //Kích thước của điểm chạm (không phải đuôi) là max
-          );
+      //   if (isHead) {
+      //     lifeLeft = 1.0; 
+      //     point.creationTime = currentTime; 
+      //     final Gradient headGradient = RadialGradient(
+      //       colors: [
+      //         // const Color.fromARGB(255,251,251,251),
+      //         // const Color.fromARGB(255,255,163,34),
+      //         // const Color.fromARGB(255,187,20,20),
+      //         // const Color.fromARGB(255, 0, 58, 107),
+      //         const Color.fromARGB(255,251,251,251),
+      //         const Color.fromARGB(150,251,251,251),
+      //         const Color.fromARGB(0,251,251,251),
+      //       ],
+      //       //stops: const [0.0, 0.0, 2.0], 
+      //     );
 
-          canvas.drawCircle(point.position, MAX_CURSOR_SIZE, paint);
-          continue;
-        } else {   //đuôi
+      //     paint.shader = headGradient.createShader(
+      //       Rect.fromCircle(center: point.position, radius: MAX_CURSOR_SIZE), //Kích thước của điểm chạm (không phải đuôi) là max
+      //     );
 
-          double ageRatio = (currentTime - point.creationTime) / timeToLive;
-          lifeLeft = 1.0 - ageRatio.clamp(0.0, 1.0); 
+      //     canvas.drawCircle(point.position, MAX_CURSOR_SIZE, paint);
+      //     continue;
+      //   } else {   //đuôi
 
-          if (lifeLeft <= 0) continue; 
+      //     double ageRatio = (currentTime - point.creationTime) / timeToLive;
+      //     lifeLeft = 1.0 - ageRatio.clamp(0.0, 1.0); 
 
-          // double currentRadius = 0.8 * MAX_CURSOR_SIZE * lifeLeft; 
+      //     if (lifeLeft <= 0) continue; 
 
-          // // final Gradient trailGradient = RadialGradient(
-          // //   colors: [
-          // //     const Color.fromARGB(255,251,251,251),
-          // //     const Color.fromARGB(255, 0, 58, 107),
-          // //   ],
-          // // );
+      //     // double currentRadius = 0.8 * MAX_CURSOR_SIZE * lifeLeft; 
 
-          // // paint.shader = trailGradient.createShader(
-          // //   Rect.fromCircle(center: point.position, radius: currentRadius),
-          // // );
-          // int transparency = (100 * lifeLeft).truncate();
-          paint.color = Color.fromARGB(150,251,251,251);
-          canvas.drawCircle(point.position, 3, paint);
+      //     // // final Gradient trailGradient = RadialGradient(
+      //     // //   colors: [
+      //     // //     const Color.fromARGB(255,251,251,251),
+      //     // //     const Color.fromARGB(255, 0, 58, 107),
+      //     // //   ],
+      //     // // );
+
+      //     // // paint.shader = trailGradient.createShader(
+      //     // //   Rect.fromCircle(center: point.position, radius: currentRadius),
+      //     // // );
+      //     // int transparency = (100 * lifeLeft).truncate();
+      //     paint.color = Color.fromARGB(150,251,251,251);
+      //     canvas.drawCircle(point.position, 3, paint);
+      //   }
+      // }
+      Path trailPath = Path();
+      if (trail.isNotEmpty) {
+        //bắt đầu từ điểm trail[0] và nối tới đầu
+        trailPath.moveTo(trail[0].position.dx, trail[0].position.dy);
+        for (int i = 1; i < trail.length; i++) {
+          trailPath.lineTo(trail[i].position.dx, trail[i].position.dy);
         }
+      }
+
+      Paint pathPaint = Paint()
+      ..color = Color.fromARGB(150, 251, 251, 251)// Opacity áp dụng đều cho cả đường
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 4.0
+      ..strokeJoin = StrokeJoin.round // Bo tròn các góc cua
+      ..strokeCap = StrokeCap.round;
+      canvas.drawPath(trailPath, pathPaint);
+
+      
+      // for (int i = 0; i < trail.length - 1; i++) {
+      //   // TrailPoint point1 = trail[i];
+      //   // TrailPoint point2 = trail[i + 1];
+        
+      //   // Paint dynamicPaint = Paint()
+      //   // ..color = Color.fromARGB(150,251,251,251)
+      //   // ..style = PaintingStyle.fill
+      //   // ..strokeCap = StrokeCap.butt
+      //   // ..strokeWidth = 20.0;
+
+      //   // paint.color = Color.fromARGB(150,251,251,251);
+      //   // paint.strokeWidth = 20;
+
+      //   Paint pathPaint = Paint()
+      //   ..color = Color.fromARGB(100, 251, 251, 251)// Opacity áp dụng đều cho cả đường
+      //   ..style = PaintingStyle.stroke
+      //   ..strokeWidth = 4.0
+      //   ..strokeJoin = StrokeJoin.round // Bo tròn các góc cua
+      //   ..strokeCap = StrokeCap.round;
+      //   canvas.drawPath(trailPath, pathPaint);
+
+      //   //canvas.drawLine(point1.position, point2.position, dynamicPaint);
+      // }
+      if (isCurrentlyPressed && trail.isNotEmpty) {
+        TrailPoint headPoint = trail.last;
+        
+        final Gradient headGradient = RadialGradient(
+          colors: [
+            // const Color.fromARGB(255,251,251,251),
+            // const Color.fromARGB(255,255,163,34),
+            // const Color.fromARGB(255,187,20,20),
+            // const Color.fromARGB(255, 0, 58, 107),
+            const Color.fromARGB(255,251,251,251),
+            const Color.fromARGB(150,251,251,251),
+            const Color.fromARGB(0,251,251,251),
+          ],
+          //stops: const [0.0, 0.0, 2.0], 
+        );
+        paint.shader = headGradient.createShader(
+          Rect.fromCircle(center: headPoint.position, radius: MAX_CURSOR_SIZE), //Kích thước của điểm chạm (không phải đuôi) là max
+        );
+
+        canvas.drawCircle(headPoint.position, MAX_CURSOR_SIZE, paint);
       }
     }
   }
--- lib/ui/touch_detector.dart
+++ lib/ui/touch_detector.dart
@@ -15,19 +15,19 @@ class TouchDetector extends StatefulWidget {
   }
 }
 
-class _TouchDetectorState extends State<TouchDetector> with SingleTickerProviderStateMixin {
+class _TouchDetectorState extends State<TouchDetector> with SingleTickerProviderStateMixin {      //SingleTickerProviderStateMixin để dùng vsync: this
   final Map<int, List<TrailPoint>> _fingers = {};
   late AnimationController _ticker;               // Khi khởi tạo _ticker và gọi hàm .repeat(), Mỗi lần gõ nhịp, sẽ gọi CustomPainter vẽ lại đuôi
   final Set<int> _activePointers = {};
 
   @override
-  void initState() {
+  void initState() {                        //được gọi một lần duy nhất và không bao giờ chạy lại. Đây là nơi tiêu chuẩn và an toàn nhất để bạn khởi tạo các biến hoặc bộ điều khiển hoạt động độc lập với quá trình vẽ giao diện (như bộ đếm thời gian ticker).
     super.initState();
     // Ticker chạy liên tục làm nhịp đập cho CustomPainter
     _ticker = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    )..repeat();
+      vsync: this,  //ngăn chặn các hoạt ảnh chạy ngầm tiêu tốn tài nguyên (như RAM, CPU) khi màn hình chứa widget này bị ẩn đi
+      duration: const Duration(seconds: 1),       //biến _ticker.value sẽ chạy từ 0.0 lên 1.0 trong đúng 1 giây, sau đó reset về 0.0 và chạy lại.
+    )..repeat();  //Hàm này ra lệnh cho bộ đếm chạy tuần hoàn vô tận yêu cầu CustomPainter vẽ lại màn hình
   }
 
   @override
@@ -37,22 +37,32 @@ class _TouchDetectorState extends State<TouchDetector> with SingleTickerProvider
   }
 
   void _handlePointerDown(PointerEvent event) {
-    _activePointers.add(event.pointer); // Đánh dấu ngón này đang giữ
-    _fingers.putIfAbsent(event.pointer, () => []);
-    _fingers[event.pointer]!.add(TrailPoint(event.localPosition, DateTime.now().millisecondsSinceEpoch));
-    _triggerUpdate();
+    setState(() {
+      _activePointers.add(event.pointer); // Đánh dấu ngón này đang giữ
+      if (_fingers.containsKey(event.pointer) == false) {
+        _fingers[event.pointer] = []; 
+      }
+      _fingers[event.pointer]!.add(TrailPoint(event.localPosition, DateTime.now().millisecondsSinceEpoch));
+      _triggerUpdate();
+    });
+    
   }
 
   void _handlePointerMove(PointerEvent event) {
-    if (_activePointers.contains(event.pointer)) {
-      _fingers[event.pointer]!.add(TrailPoint(event.localPosition, DateTime.now().millisecondsSinceEpoch));
-      _triggerUpdate();
-    }
+    setState(() {
+      if (_activePointers.contains(event.pointer)) {
+        _fingers[event.pointer]!.add(TrailPoint(event.localPosition, DateTime.now().millisecondsSinceEpoch));
+        _triggerUpdate();
+      }
+    });
   }
 
   void _handlePointerUp(PointerEvent event) {
-    _activePointers.remove(event.pointer); // chỉ bỏ đánh dấu đang giữ, KHÔNG xóa khỏi _fingers
-    _triggerUpdate();
+    setState(() {
+      _activePointers.remove(event.pointer); // chỉ bỏ đánh dấu đang giữ, KHÔNG xóa khỏi _fingers
+      _triggerUpdate();
+    });
+    
   }
 
   // Hàm _triggerUpdate  chỉ lấy tọa độ của các ngón đang giữ
@@ -63,35 +73,158 @@ class _TouchDetectorState extends State<TouchDetector> with SingleTickerProvider
         currentPoints[id] = _fingers[id]!.last.position;
       }
     }
+        
     widget.onUpdate(currentPoints);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Listener(
-      //Bắt buộc phải có
-      behavior: HitTestBehavior.opaque,
-      
-      //Khi nhấn 1 ngón
-      onPointerDown: _handlePointerDown,
-      onPointerMove: _handlePointerMove,
-      onPointerUp: _handlePointerUp,
-      onPointerCancel: _handlePointerUp,
+    Map<int, Offset> currentPoints = {};
+    for (int id in _activePointers) {
+      if (_fingers[id] != null && _fingers[id]!.isNotEmpty) {
+        currentPoints[id] = _fingers[id]!.last.position;
+      }
+    }
+    return Stack(
+      children: [
+        Listener(
+          //Bắt buộc phải có
+          behavior: HitTestBehavior.opaque,
+          
+          //Khi nhấn 1 ngón
+          onPointerDown: _handlePointerDown,
+          onPointerMove: _handlePointerMove,
+          onPointerUp: _handlePointerUp,
+          onPointerCancel: _handlePointerUp,
 
-      child: Container(
-        //color: Color.fromARGB(255,228,244,252),
-        color: Color.fromARGB(255, 0, 0, 0),
-        child: CustomPaint(
-          painter: TrailPainter(
-            fingers: _fingers,
-            activePointers: _activePointers,
-            repaint: _ticker, // Ticker ép Painter vẽ lại liên tục 60fps
-            timeToLive: 300,  //set tốc độ shrink của đuôi
-          ),
-          size: Size.infinite,
+          child: Container(
+            //color: Color.fromARGB(255,228,244,252),
+            color: Color.fromARGB(255, 0, 0, 0),
+            child: CustomPaint(
+              painter: TrailPainter(
+                fingers: _fingers,
+                activePointers: _activePointers,
+                repaint: _ticker, // Ticker ép Painter vẽ lại liên tục 60fps
+                timeToLive: 300,  //set tốc độ shrink của đuôi
+              ),
+              size: Size.infinite,
+            ),
+          )
+          
         ),
-      )
-      
+
+
+        Positioned(
+          top: 50, // Cách mép trên 50 pixel (tránh bị dính vào thanh trạng thái pin/wifi)
+          left: 250, // Cách mép trái 20 pixel
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            color: Color.fromARGB(144, 255, 255, 255),
+            child: Text(
+              "Số ngón: (${_activePointers.length})",
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+
+
+        if (_activePointers.isEmpty) 
+          Positioned(
+            top: 50, // Cách mép trên 50 pixel (tránh bị dính vào thanh trạng thái pin/wifi)
+            left: 20, // Cách mép trái 20 pixel
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              color: Color.fromARGB(144, 255, 255, 255),
+              child: Text(
+                "Waiting for input...",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        
+        
+        if (_activePointers.length == 1)
+          Positioned(
+            top: 50, // Cách mép trên 50 pixel (tránh bị dính vào thanh trạng thái pin/wifi)
+            left: 20, // Cách mép trái 20 pixel
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              color: Color.fromARGB(144, 255, 255, 255),
+              child: Text(
+                //"Tọa độ: (${_fingers.values.elementAt(_activePointers.elementAt(0)).last.position})",
+                //"Tọa độ: (${_fingers.entries..last.position})",
+                "Tọa độ 1: (${currentPoints.entries.elementAt(0).value.dx.round()}, ${currentPoints.entries.elementAt(0).value.dy.round()})",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        
+        if (_activePointers.length == 2)
+          Positioned(
+            top: 50, // Cách mép trên 50 pixel (tránh bị dính vào thanh trạng thái pin/wifi)
+            left: 20, // Cách mép trái 20 pixel
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              color: Color.fromARGB(144, 255, 255, 255),
+              child: Text(
+                "Tọa độ 1: (${currentPoints.entries.elementAt(0).value.dx.round()}, ${currentPoints.entries.elementAt(0).value.dy.round()}),\nTọa độ 2: (${currentPoints.entries.elementAt(1).value.dx.round()}, ${currentPoints.entries.elementAt(1).value.dy.round()})",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+
+        if (_activePointers.length == 3)
+          Positioned(
+            top: 50, // Cách mép trên 50 pixel (tránh bị dính vào thanh trạng thái pin/wifi)
+            left: 20, // Cách mép trái 20 pixel
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              color: Color.fromARGB(144, 255, 255, 255),
+              child: Text(
+                "Tọa độ 1: (${currentPoints.entries.elementAt(0).value.dx.round()}, ${currentPoints.entries.elementAt(0).value.dy.round()})\nTọa độ 2: (${currentPoints.entries.elementAt(1).value.dx.round()}, ${currentPoints.entries.elementAt(1).value.dy.round()}),\nTọa độ 3: (${currentPoints.entries.elementAt(2).value.dx.round()}, ${currentPoints.entries.elementAt(2).value.dy.round()})",
+                style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        
+        if (_activePointers.length == 4)
+          Positioned(
+            top: 50, // Cách mép trên 50 pixel (tránh bị dính vào thanh trạng thái pin/wifi)
+            left: 20, // Cách mép trái 20 pixel
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              color: Color.fromARGB(144, 255, 255, 255),
+              child: Text(
+                  "Tọa độ 1: (${currentPoints.entries.elementAt(0).value.dx.round()}, ${currentPoints.entries.elementAt(0).value.dy.round()})\nTọa độ 2: (${currentPoints.entries.elementAt(1).value.dx.round()}, ${currentPoints.entries.elementAt(1).value.dy.round()}),\nTọa độ 3: (${currentPoints.entries.elementAt(2).value.dx.round()}, ${currentPoints.entries.elementAt(2).value.dy.round()}),\nTọa độ 4: (${currentPoints.entries.elementAt(2).value.dx.round()}, ${currentPoints.entries.elementAt(2).value.dy.round()})",
+                  style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+      ],
     );
   }
 }
